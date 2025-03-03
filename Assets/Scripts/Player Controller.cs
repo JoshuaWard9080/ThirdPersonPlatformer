834eb5b851097d3697623d90923becada296da94
JoshuaWard9080 <144284996+JoshuaWard9080@users.noreply.github.com>
2025-03-02 17:46:29 -0800

    Player Movement Fixed
    Player movement was giving A LOT of grief and I have no idea why. Cinemachine was going wonky and staying extremely zoomed in. Player movement was MUCH faster in the horizontal component than the vertical component (in raw input terms). Things were bugging out. Re-started Unity and re-pulled from main and returned the stashed changes and everything is working fine now. I can only imagine this is a Super Mario Speedrun situation where solar radiation flipped a bit in my computer and caused everything to go weird.
    
    As for actual changes, the player moves in the direction that the camera is facing. Added drag variable. Added boolean to check if the player is touching the ground, which is checked via Raycast. Added a function to make sure that the player's speed matches the playerSpeed function.

--- Assets/Scripts/Player Controller.cs
+++ Assets/Scripts/Player Controller.cs
@@ -3,10 +3,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Transform orientation;
     [SerializeField] private InputManager inputManager;
     [SerializeField] private float playerSpeed;
+    [SerializeField] private float groundDrag;
+    [SerializeField] private float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+
+    Vector3 moveDir;
 
     private Rigidbody rb;
+
+    
 
     public void Start()
     {
@@ -14,8 +23,32 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
+    public void Update()
+    {
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+        SpeedControl();
+
+        if (grounded)
+            rb.linearDamping = groundDrag;
+        else 
+            rb.linearDamping = 0;
+    }
+
     public void MovePlayer(Vector3 input)
     {
-        rb.AddForce(input * playerSpeed);
+        moveDir = orientation.forward * input.z + orientation.right * input.x;
+        rb.AddForce(moveDir.normalized * playerSpeed, ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+        if (flatVelocity.magnitude > playerSpeed)
+        {
+            Vector3 limitedVelocity = flatVelocity.normalized * playerSpeed;
+            rb.linearVelocity = new Vector3(limitedVelocity.x, rb.linearVelocity.y, limitedVelocity.z);
+        }
     }
 }
